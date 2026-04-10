39f8bd0c2c3b38db3ab074e819acfa6840fbd5a2
Gallagowtham <125870179+Gallagowtham@users.noreply.github.com>
2026-04-10 11:44:41 +0530

    Add TestData class with user and payment details

--- utils/test_data.py
+++ utils/test_data.py
@@ -1,1 +1,10 @@
-payment_amount = "100"
+class TestData:
+
+    USERNAME = "standard_user"
+    PASSWORD = "secret_sauce"
+
+    FIRST_NAME = "Gowtham"
+    LAST_NAME = "QA"
+    ZIP_CODE = "517001"
+
+    PAYMENT_AMOUNT = "100"
